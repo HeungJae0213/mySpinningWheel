087f892a448413bdbb196fd37588a31c29c8b6f6
choheungjae <heung@ubisam.com>
2026-08-27 09:34:30 +0900

    20260827 sdk 3.x 버전으로 업그레이드

--- apps-in-toss.config.ts
+++ apps-in-toss.config.ts
@@ -9,20 +9,12 @@ dotenv.config();
 
 export default defineConfig({
   appName: 'my-spinning-wheel',
+
   brand: {
-    displayName: '돌림판', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
-    primaryColor: '#3182F6', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: process.env.VITE_APP_ICON || '',
-    bridgeColorMode: 'basic',
+    // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
+    primaryColor: '#3182F6'
   },
-  web: {
-    host: process.env.VITE_APP_HOST || '192.168.0.10', // 모바일 환경에서 접속하려면 PC의 로컬 IP 주소 사용
-    port: 5173,
-    commands: {
-      dev: 'vite --host',
-      build: 'vite build',
-    },
-  },
+
   permissions: [],
-  outdir: 'dist',
+  webBundleDir: 'dist'
 });
